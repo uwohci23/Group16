92c35a6a3ee5acbd7b31522e34150527bc5fc162
Elijah Joseph John Deluzio <edeluzio@uwo.ca>
2023-03-31 17:24:30 -0400

    Modifies package files to remove grunt related packages

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './js/Main.js',
+  mode: "development",
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
